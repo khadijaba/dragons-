281ca5f1a51d5335423c460482bbce1af6768315
raouaagara <159240404+raouaagara@users.noreply.github.com>
2024-03-07 19:01:16 +0100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "main_equipement.h"
+#include <QApplication>
+#include <QMessageBox>
+#include "connection.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    Connection c;
+    bool test = c.createconnect();
+    MainWindow w;
+    if (test) {
+
+        w.show();
+
+        QMessageBox::information(nullptr, QObject::tr("Database is open"),
+                                 QObject::tr("Connection successful.\n"
+                                             "Click Cancel to exit."), QMessageBox::Cancel);
+    } else {
+        QMessageBox::critical(nullptr, QObject::tr("Database is not open"),
+                              QObject::tr("Connection failed.\n"
+                                          "Click Cancel to exit."), QMessageBox::Cancel);
+    }
+
+    return a.exec();
+}
