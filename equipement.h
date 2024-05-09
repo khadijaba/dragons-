9aced075acc2378cc97dd3351a0c3f7edb60b7e5
raouaagara <159240404+raouaagara@users.noreply.github.com>
2024-05-09 19:08:30 +0100

    equipement.h

--- equipement.h
+++ equipement.h
@@ -1,28 +1,49 @@
 #ifndef EQUIPEMENT_H
 #define EQUIPEMENT_H
+
+#include <QObject>
 #include <QString>
-#include <QSqlQueryModel>
-
+#include <QList>
+#include <QtNetwork/QSslError>
 #include <QDate>
+#include <QSqlQueryModel>
+#include <QObject>
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#include <QtCore/QList>
+#include <QPrinter>
+#include <QTextDocument>
+#include <QTextCursor>
+#include "qr.h"
+#include <QSqlDatabase>
 
-class Equipement
+class Equipement : public QObject
 {
+    Q_OBJECT
 public:
     Equipement();
-    Equipement(int,QDate,QString,QString);
-    int get_ID();
-    QString get_Type();
-    QString get_Prix();
-    QDate get_Date();
+    Equipement(int, QDate, QString, QString);
+    int get_ID() const;
+    QString get_Type()const;
+    QString get_Prix()const;
+    QDate get_Date()const;
 
     void set_ID(int);
     void set_Type(QString);
     void set_Prix(QString);
     void set_Date(QDate);
+    //void:genererEtAfficherCodeQR(const Equipement& equipement)
+    bool exists(int id);
+    QSqlQueryModel* search(QString id);
     bool Ajouter();
     bool modifier();
     QSqlQueryModel* afficher();
-     bool equipementExiste(int ID);
+    QSqlQueryModel* statistiqueEquipement();
+    void envoyerEmail(const QString &destinataire, const QString &sujet, const QString &contenu);
+    void on_pb_pdf_clicked();
+    bool generatePdf_Equipement(const QString &sortingColumn, const QString &sortOrder);
+    bool importer_pdf(QPrinter &printer);
+    bool equipementExiste(int ID);
     bool supprimer(int);
     bool RechercheEquipementParID(int IDrecherche);
     QSqlQueryModel *tri_ID();
@@ -31,6 +52,8 @@ public:
     QSqlQueryModel *tri_type();
 
 
+public slots:
+
 
 
 private:
@@ -38,9 +61,7 @@ private:
     QDate datee;
     QString prix;
     QString type;
-
-
-
 };
 
-#endif // EQUIPEMENT_H
+#endif
+// EQUIPEMENT_H
